161aaff6ac6f873616393c4876996c69f3bba775
Andrew Zhang <azhang377@gatech.edu>
2022-08-01 16:22:38 -0400

    enable adding shows to list, update stats/appearance in menu

--- lib/list_items.dart
+++ lib/list_items.dart
@@ -1,15 +1,23 @@
 class ShowList {
   String name;
-  int length = 0;
+  late int length;
   var shows = <Show>[];
 
-  ShowList({required this.name});
+  ShowList({required this.name}) {
+    length = 0;
+  }
+
+  void addShow(Show newShow) {
+    shows.add(newShow);
+    length ++;
+  }
 }
 
 class Show {
   String name;
   late int score;
   late String comments;
+  late bool watched = false;
   
   Show({required this.name});
 }
